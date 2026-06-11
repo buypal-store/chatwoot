0060ed84bd4ad82279efe67487f239c2da4d1a5e
buypal-store <yazidchahua10@hotmail.com>
2026-06-10 20:14:14 -0500

    Change values structure to include id and name

--- app/javascript/dashboard/components/ChatList.vue
+++ app/javascript/dashboard/components/ChatList.vue
@@ -583,7 +583,7 @@ function onUrgentFilter() {
   customAttributeType: '',
   filterOperator: 'equal_to',
   queryOperator: null,
-  values: [currentUserId],
+  values: [{ id: currentUserId, name: '' }],
 },
   ];
   resetBulkActions();

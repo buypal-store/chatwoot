5a054879155073fb8f52819d50ca453322a0339c
buypal-store <yazidchahua10@hotmail.com>
2026-08-28 11:53:58 -0500

    Add last_message_desc sorting option

--- app/finders/conversation_finder.rb
+++ app/finders/conversation_finder.rb
@@ -11,6 +11,7 @@ class ConversationFinder
     'priority_desc' => %w[sort_on_priority desc],
     'waiting_since_asc' => %w[sort_on_waiting_since asc],
     'waiting_since_desc' => %w[sort_on_waiting_since desc],
+    'last_message_desc' => %w[sort_on_last_message desc],
     'priority_desc_created_at_asc' => %w[sort_on_priority_created_at desc],
     'unread' => %w[sort_on_unread desc],
 

56e7d1637bfa8ec6ae388ef62560ebe82b2d20a4
buypal-store <yazidchahua10@hotmail.com>
2026-08-30 16:12:28 -0500

    Refactor sort_on_last_message method for clarity

--- app/models/concerns/sort_handler.rb
+++ app/models/concerns/sort_handler.rb
@@ -32,12 +32,10 @@ module SortHandler
       order('grouped_conversations.message_type', 'grouped_conversations.created_at ASC')
     end
     
-    def sort_on_last_message(_sort_direction = :desc)
+    def sort_on_last_message(sort_direction = :desc)
       order(
         generate_sql_query(
-          '(SELECT MAX(m.created_at) FROM messages m ' \
-          'WHERE m.conversation_id = conversations.id ' \
-          'AND m.message_type IN (0,1) AND m.private = false) DESC NULLS LAST'
+          "last_message_at #{sort_direction.to_s.upcase} NULLS LAST, created_at DESC"
         )
       )
     end
